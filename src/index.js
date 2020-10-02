201d9db2f6b3da24fae8539223bda49cde0adcad
Alexandr Pilipenko <vopilipenko@gmail.com>
2020-10-02 10:57:04 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let min = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] <= min) {
+                min = array[i];
+            } else {
+                min = min;
+            }
+        }
+        return min;
+    }
+};
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let max = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] >= max) {
+                max = array[i];
+            } else {
+                max = max;
+            }
+        }
+        return max;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        array.forEach((element, index, array) => {
+            sum = sum + element;
+        });
+        return sum / array.length;
+    }
+};
